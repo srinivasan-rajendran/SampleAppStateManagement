da13c88259b5757d37cd282a129bb47cab0d60ce
Srinivasan Rajendran <inforsrinivasan@gmail.com>
2020-10-05 19:35:15 +0200

    Initial Commit

---
+++ PointFree/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PointFree
+//
+//  Created by Srinivasan Rajendran on 2020-10-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
